2babd55e06901d3fc881d67f45c92146861dc082
JP <jp@jp.nl>
2021-09-13 15:34:34 +0200

    Script defer/async verteld

---
+++ demo/script/dom-dingen.js
@@ -0,0 +1,30 @@
+// deze gaat af als de HTML geparset is
+// (hetzelfde als het defer-attribuut)
+// window.addEventListener('DOMContentLoaded', () => {
+// 	let h1 = document.querySelector('h1');
+// 	console.log('h1:', h1);
+
+// 	console.log(h1.innerText);
+// });
+
+// // deze gaat als af de gehele pagina klaar is met laden
+// // inclusief plaatjes, videos, flashobjecten, advertenties, iframes, ...
+// window.addEventListener('load', () => {
+// 	let h1 = document.querySelector('h1');
+// 	console.log('h1:', h1);
+
+// 	console.log(h1.innerText);
+
+// });
+
+let h1 = document.querySelector('h1');
+console.log('h1:', h1);
+
+console.log(h1.innerText);
+
+// jQuery
+// $('table tbody').addClass('...');
+// $('<tr>').appendTo('body')
+// $(function() { // DOM ready function
+
+// });
